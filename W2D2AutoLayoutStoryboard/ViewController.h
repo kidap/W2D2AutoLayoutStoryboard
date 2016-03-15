f317551839bb214341d3bc4ba524d17d13ec1104
Karlo Pagtakhan <kidap@yahoo.com>
2016-03-15 14:02:18 -0400

    Add Ggreen, purple, and blue boxes

---
+++ W2D2AutoLayoutStoryboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W2D2AutoLayoutStoryboard
+//
+//  Created by Karlo Pagtakhan on 03/15/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
